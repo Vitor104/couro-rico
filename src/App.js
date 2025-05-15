77191653fbc257c379571de6216c848ba2876d54
João Vitor <122707909+Vitor104@users.noreply.github.com>
2025-05-14 21:52:32 -0300

    fiz algumas melhorias

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
+import Hero from './components/hero/hero';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/hero" element={<Hero />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,16 +2,12 @@ import React from "react";
 import styles from "./home.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import bolsa from './bag-1052370_1920.jpg';
-import Hero from "../hero/hero";
-import { NavLink } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 
 function Home() {
   return (
     <div className={styles.container}>
-
-      <a href="../hero/hero">Hgagfa</a>
       
       <nav className={`navbar navbar-expand-lg ${styles.navPerso}`}>
         <NavLink to="#" className={styles.logo}>Couro Rico</NavLink>
@@ -25,7 +21,7 @@ function Home() {
                 <NavLink to="#" className="nav-link">Inicio</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/Hero" className="nav-link">Produtos</NavLink>
+                <NavLink exact to={"/hero"} className="nav-link">Produtos</NavLink>
               </li>
               <li className="nav-item">
                 <NavLink to="#" className="nav-link">Sobre</NavLink>
@@ -38,20 +34,34 @@ function Home() {
           </div>
         </nav>
        
-
-
-        <div class="media">
-  
-  <div class="media-body bg-light">
-    <h5 class="mt-0">Top-aligned media</h5>
-    <img src={bolsa} className={`align-self-start mr-3 ${styles.fotos}`} alt="..."></img>
-    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-    <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-  </div>
-</div>
     </div>
+
   );
+  
+
 }
 
 
 export default Home;
+
+
+
+/* 
+<ul className="navbar-nav">
+              <li className="nav-item active">
+                <NavLink to="#" className="nav-link">Inicio</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink exact to={"/Hero"} className="nav-link">Produtos</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="#" className="nav-link">Sobre</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="#" className="nav-link">Contatos</NavLink>
+              </li>
+            </ul>
+
+
+
+*/
